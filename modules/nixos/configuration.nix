16aa8e8a9da9a5a3c92a3dd15466953663e579e4
Drigster <62962772+Drigster@users.noreply.github.com>
2024-09-22 14:23:56 +0300

    89 current  2024-09-22 14:23:36  24.11.20240906.574d1ea   6.6.49                          *

--- modules/nixos/configuration.nix
+++ modules/nixos/configuration.nix
@@ -131,6 +131,13 @@
     # here, NOT in environment.systemPackages
   ];
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+
   nixpkgs.config.permittedInsecurePackages = [
     "openssl-1.1.1w"
   ];
