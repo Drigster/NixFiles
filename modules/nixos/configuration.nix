a39137598e2d5f384db4da285138f782b47063f2
Drigster <62962772+Drigster@users.noreply.github.com>
2024-09-22 14:47:27 +0300

    96 current  2024-09-22 14:47:17  24.11.20240906.574d1ea   6.6.49                          *

--- modules/nixos/configuration.nix
+++ modules/nixos/configuration.nix
@@ -67,6 +67,25 @@
     pulse.enable = true;
   };
 
+  systemd = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = ["graphical-session.target"];
+      wants = ["graphical-session.target"];
+      after = ["graphical-session.target"];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+    extraConfig = ''
+      DefaultTimeoutStopSec=10s
+    '';
+  };
+
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
