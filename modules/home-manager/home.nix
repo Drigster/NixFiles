762cabbaef0a672b58b5672f76f2061d7f2a48ad
Drigster <62962772+Drigster@users.noreply.github.com>
2024-10-23 17:40:53 +0300

    141 current  2024-10-23 17:39:10  24.11.20240906.574d1ea   6.6.49                          *

--- modules/home-manager/home.nix
+++ modules/home-manager/home.nix
@@ -50,6 +50,7 @@
 
     pkgs.prismlauncher
     pkgs.telegram-desktop
+    pkgs.arc-icon-theme
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
