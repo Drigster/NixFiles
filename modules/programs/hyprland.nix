2a63816cac02058f55f4392759d743ef424d389d
Drigster <62962772+Drigster@users.noreply.github.com>
2024-11-06 17:41:59 +0200

    153 current  2024-11-06 17:38:52  24.11.20240906.574d1ea   6.6.49                          *

--- modules/programs/hyprland.nix
+++ modules/programs/hyprland.nix
@@ -121,10 +121,10 @@
         "workspace 5 silent, class:^([Ss]team)$, title:^([Ss]team)$"
         "float, class:^([Ss]team)$, title:^((?![Ss]team).*)$"
         "tile, class:^([Ss]team)$, title:^([Ss]team)$"
-        "workspace 1 silent, class:^(steam_app_(.*))$"
+        "workspace 1, class:^(steam_app_(.*))$"
         "fullscreen, class:^(steam_app_(.*))$"
-        "workspace 1 silent, class:^(factorio)$"
-        "fullscreen, class:^(factorio)$"
+        # "workspace 1, class:^(factorio)$"
+        # "fullscreen, class:^(factorio)$"
 
         #Thunar
         "float,class:^(thunar)$,title:^(File Operation Progress)$"
