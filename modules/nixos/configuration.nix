50b251c64438733a5b6bd8e881cfe5d937623b11
Drigster <62962772+Drigster@users.noreply.github.com>
2024-11-28 17:36:25 +0200

    72 current  2024-11-28 17:02:39  24.11.20241116.057f63b   6.6.60                          *

--- modules/nixos/configuration.nix
+++ modules/nixos/configuration.nix
@@ -65,6 +65,16 @@
 
   security.rtkit.enable = true;
   security.polkit.enable = true;
+  security.pam.services = {
+    sddm.text = ''
+      auth 			[success=1 new_authtok_reqd=1 default=ignore]  	pam_unix.so try_first_pass likeauth nullok
+      auth 			sufficient  	pam_fprintd.so
+      auth      substack      login
+      account   include       login
+      password  substack      login
+      session   include       login
+    '';
+  };
 
   services.pipewire = {
     enable = true;
