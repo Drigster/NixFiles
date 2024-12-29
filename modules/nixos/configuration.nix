c6bd5607f8e03b144ef6e542c818e3bb58c51164
Drigster <62962772+Drigster@users.noreply.github.com>
2024-12-30 00:36:55 +0200

    111 current  2024-12-30 00:35:19  24.11.20241116.057f63b   6.6.60                          *

--- modules/nixos/configuration.nix
+++ modules/nixos/configuration.nix
@@ -59,8 +59,20 @@
   services.gvfs.enable = true;
 
   hardware.pulseaudio.enable = false;
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = true;
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        Name = "Computer";
+        ControllerMode = "dual";
+        FastConnectable = "true";
+        Experimental = "true";
+      };
+      Policy = {AutoEnable = "true";};
+      LE = {EnableAdvMonInterleaveScan = "true";};
+    };
+  };
   hardware.graphics.enable = true;
 
   security.rtkit.enable = true;
@@ -76,15 +88,16 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-  };
-
-  services.pipewire.wireplumber.extraConfig.bluetoothEnhancements = {
-    "monitor.bluez.properties" = {
-      "bluez5.enable-sbc-xq" = true;
-      "bluez5.enable-msbc" = true;
-      "bluez5.enable-hw-volume" = true;
-      "bluez5.roles" = ["hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag"];
-    };
+    wireplumber.configPackages = [
+      (pkgs.writeTextDir "share/wireplumber/wireplumber.conf.d/10-bluez.conf" ''
+        monitor.bluez.properties = {
+          bluez5.roles = [ a2dp_sink a2dp_source bap_sink bap_source hsp_hs hsp_ag hfp_hf hfp_ag ]
+          bluez5.codecs = [ sbc sbc_xq aac ]
+          bluez5.enable-sbc-xq = true
+          bluez5.hfphsp-backend = "native"
+        }
+      '')
+    ];
   };
 
   services.syncthing = {
