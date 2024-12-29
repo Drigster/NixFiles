d92a5267147f8f6ffc36467c16f67d9cc316f4b5
Drigster <62962772+Drigster@users.noreply.github.com>
2024-12-29 22:27:39 +0200

    106 current  2024-12-29 22:26:04  24.11.20241116.057f63b   6.6.60                          *

--- modules/nixos/configuration.nix
+++ modules/nixos/configuration.nix
@@ -78,6 +78,27 @@
     pulse.enable = true;
   };
 
+  services.pipewire.wireplumber.extraConfig.bluetoothEnhancements = {
+    "monitor.bluez.properties" = {
+      "bluez5.enable-sbc-xq" = true;
+      "bluez5.enable-msbc" = true;
+      "bluez5.enable-hw-volume" = true;
+      "bluez5.roles" = ["hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag"];
+    };
+  };
+
+  services.syncthing = {
+    enable = true;
+    user = "drigster";
+    dataDir = "/home/drigster/Documents";
+    configDir = "/home/drigster/.config/syncthing";
+  };
+
+  services.syncthing.settings.gui = {
+    user = "drigster";
+    password = "drigster";
+  };
+
   systemd = {
     user.services.polkit-gnome-authentication-agent-1 = {
       description = "polkit-gnome-authentication-agent-1";
